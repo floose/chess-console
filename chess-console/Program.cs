9ab3550768abea8bd43abd960108ad7e814df264
Felipe <felipe.loose@gmail.com>
2023-01-06 21:47:27 +0100

    changed object Board name in ChessMatch. Started first row of pieces in the startmatch project.

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -5,12 +5,13 @@ using board.Enums;
 using chess;
 using exceptions;
 
-ChessMatch match = new ChessMatch();
+try
+{
+    ChessMatch match = new ChessMatch();
+    Screen.PrintBoard(match.Board);
 
-Screen.PrintBoard(match.getBoard());
-
-match.makeMove(new Position(0, 0), new Position(2, 3));
-
-Console.WriteLine();
-
-Screen.PrintBoard(match.getBoard());
+}
+catch(BoardException exception)
+{
+    Console.WriteLine(exception.Message);
+}
--- chess-console/chess/ChessMatch.cs
+++ chess-console/chess/ChessMatch.cs
@@ -1,40 +1,62 @@
 ﻿using board;
 using board.Enums;
+using System.Runtime;
 
 namespace chess
 {
     internal class ChessMatch
     {
-        private Board ChessBoard;
+        public Board Board { get; private set; }
         private int Turn;
         private Color ActualPlayer;
+        public bool MatchEnded { get; private set; }
 
         public ChessMatch()
         {
-            ChessBoard = new Board(8, 8);
+            Board = new Board(8, 8);
             Turn = 1;
             ActualPlayer = Color.White;
+            MatchEnded = false;
             StartBoard();
         }
 
         public void makeMove(Position origin, Position destination)
         {
             
-            Piece piece = ChessBoard.RemovePiece(origin); //stores the piece of the origin position
+            Piece piece = Board.RemovePiece(origin); //stores the piece of the origin position
             piece.IncrementNumberOfMoves();               //increments number of move of such piece
-            ChessBoard.RemovePiece(destination);         //stores and removes the piece of destination
-            ChessBoard.SetPiece(piece, destination);    //sets the piece at origin to its destination
+            Board.RemovePiece(destination);         //stores and removes the piece of destination
+            Piece capturedPiece = Board.RemovePiece(destination); //takes the piece from destination
+            Board.SetPiece(piece, destination);    //sets the piece at origin to its destination
         }
 
+        //puts the chess pieces into the board (manually)
         private void StartBoard()
         {
-            ChessBoard.SetPiece(new Rook(Color.White, ChessBoard), new Position(0, 0));
+            //White Pieces
+            Board.SetPiece(new Rook(Color.White, Board), new PositionChess('a',1).ToPosition());
+            Board.SetPiece(new Knight(Color.White, Board), new PositionChess('b', 1).ToPosition());
+            Board.SetPiece(new Bishop(Color.White, Board), new PositionChess('c', 1).ToPosition());
+            Board.SetPiece(new Queen(Color.White, Board), new PositionChess('d', 1).ToPosition());
+            Board.SetPiece(new King(Color.White, Board), new PositionChess('e', 1).ToPosition());
+            Board.SetPiece(new Bishop(Color.White, Board), new PositionChess('f', 1).ToPosition());
+            Board.SetPiece(new Knight(Color.White, Board), new PositionChess('g', 1).ToPosition());
+            Board.SetPiece(new Rook(Color.White, Board), new PositionChess('h', 1).ToPosition());
+
+            //black pieces
+            Board.SetPiece(new Rook(Color.Black, Board), new PositionChess('a', 8).ToPosition());
+            Board.SetPiece(new Knight(Color.Black, Board), new PositionChess('b', 8).ToPosition());
+            Board.SetPiece(new Bishop(Color.Black, Board), new PositionChess('c', 8).ToPosition());
+            Board.SetPiece(new Queen(Color.Black, Board), new PositionChess('d', 8).ToPosition());
+            Board.SetPiece(new King(Color.Black, Board), new PositionChess('e', 8).ToPosition());
+            Board.SetPiece(new Bishop(Color.Black, Board), new PositionChess('f', 8).ToPosition());
+            Board.SetPiece(new Knight(Color.Black, Board), new PositionChess('g', 8).ToPosition());
+            Board.SetPiece(new Rook(Color.Black, Board), new PositionChess('h', 8).ToPosition());
+
+
         }
 
-        public Board getBoard()
-        {
-            return ChessBoard;
-        }
+
 
     }
 }
